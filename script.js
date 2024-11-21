758a1ae1958c7e6d2edb531d48f98f337a8f9bb3
bhargavaankit <bhargava.ankit40@gmail.com>
2024-11-22 02:43:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,138 @@
-var labels = ['weeks', 'days', 'hours', 'minutes', 'seconds'],
-	TimerCount = (new Date().getFullYear() + 1) + '/01/01',
-	template = _.template( jQuery('#main-example-template').html()),
-	currDate = '00:00:00:00:00',
-	nextDate = '00:00:00:00:00',
-	parser = /([0-9]{2})/gi,
-	$example = jQuery('#main-example');
+/**
+ * @name		jQuery Countdown Plugin
+ * @author		Martin Angelov
+ * @version 	1.0
+ * @url			http://tutorialzine.com/2011/12/countdown-jquery/
+ * @license		MIT License
+ */
 
-	if( $example.data("timer").length ){
-		TimerCount = $example.data("timer");	
-	}
+(function($){
+	
+	// Number of seconds in every time division
+	var days	= 24*60*60,
+		hours	= 60*60,
+		minutes	= 60;
+	
+	// Creating the plugin
+	$.fn.countdown = function(prop){
+		
+		var options = $.extend({
+			callback	: function(){},
+			timestamp	: 0
+		},prop);
+		
+		var left, d, h, m, s, positions;
 
-// Parse countdown string to an object
-function strfobj(str) {
-	var parsed = str.match(parser),
-		obj = {};
-	labels.forEach(function(label, i) {
-		obj[label] = parsed[i]
-	});
-	return obj;
-}
-// Return the time components that diffs
-function diff(obj1, obj2) {
-	var diff = [];
-	labels.forEach(function(key) {
-		if (obj1[key] !== obj2[key]) {
-			diff.push(key);
+		// Initialize the plugin
+		init(this, options);
+		
+		positions = this.find('.position');
+		
+		(function tick(){
+			
+			// Time left
+			left = Math.floor((options.timestamp - (new Date())) / 1000);
+			
+			if(left < 0){
+				left = 0;
+			}
+			
+			// Number of days left
+			d = Math.floor(left / days);
+			updateDuo(0, 1, d);
+			left -= d*days;
+			
+			// Number of hours left
+			h = Math.floor(left / hours);
+			updateDuo(2, 3, h);
+			left -= h*hours;
+			
+			// Number of minutes left
+			m = Math.floor(left / minutes);
+			updateDuo(4, 5, m);
+			left -= m*minutes;
+			
+			// Number of seconds left
+			s = left;
+			updateDuo(6, 7, s);
+			
+			// Calling an optional user supplied callback
+			options.callback(d, h, m, s);
+			
+			// Scheduling another call of this function in 1s
+			setTimeout(tick, 1000);
+		})();
+		
+		// This function updates two digit positions at once
+		function updateDuo(minor,major,value){
+			switchDigit(positions.eq(minor),Math.floor(value/10)%10);
+			switchDigit(positions.eq(major),value%10);
 		}
-	});
-	return diff;
-}
-// Build the layout
-var initData = strfobj(currDate);
-labels.forEach(function(label, i) {
-	$example.append(template({
-		curr: initData[label],
-		next: initData[label],
-		label: label
-	}));
-});
-// Starts the countdown
-$example.countdown(TimerCount, function(event) {
-	var newDate = event.strftime('%w:%d:%H:%M:%S'),
-		data;
+		
+		return this;
+	};
 
-	if (newDate !== nextDate) {
-		currDate = nextDate;
-		nextDate = newDate;
-		// Setup the data
-		data = {
-			'curr': strfobj(currDate),
-			'next': strfobj(nextDate)
-		};
-		// Apply the new values to each node that changed
-		diff(data.curr, data.next).forEach(function(label) {
-			var selector = '.%s'.replace(/%s/, label),
-				$node = $example.find(selector);
-			// Update the node
-			$node.removeClass('flip');
-			$node.find('.curr').text(data.curr[label]);
-			$node.find('.next').text(data.next[label]);
-			// Wait for a repaint to then flip
-			_.delay(function($node) {
-				$node.addClass('flip');
-			}, 50, $node);
+
+	function init(elem, options){
+		elem.addClass('countdownHolder');
+
+		// Creating the markup inside the container
+		$.each(['Days','Hours','Minutes','Seconds'],function(i){
+			$('<span class="count'+this+'">').html(
+				'<span class="position">\
+					<span class="digit static">0</span>\
+				</span>\
+				<span class="position">\
+					<span class="digit static">0</span>\
+				</span>'
+			).appendTo(elem);
+			
+			if(this!="Seconds"){
+				elem.append('<span class="countDiv countDiv'+i+'"></span>');
+			}
 		});
+
 	}
-});
+
+	// Creates an animated transition between the two numbers
+	function switchDigit(position,number){
+		
+		var digit = position.find('.digit')
+		
+		if(digit.is(':animated')){
+			return false;
+		}
+		
+		if(position.data('digit') == number){
+			// We are already showing this number
+			return false;
+		}
+		
+		position.data('digit', number);
+		
+		var replacement = $('<span>',{
+			'class':'digit',
+			css:{
+				top:'-2.1em',
+				opacity:0
+			},
+			html:number
+		});
+		
+		// The .static class is added when the animation
+		// completes. This makes it run smoother.
+		
+		digit
+			.before(replacement)
+			.removeClass('static')
+			.animate({top:'2.5em',opacity:0},'fast',function(){
+				digit.remove();
+			})
+
+		replacement
+			.delay(100)
+			.animate({top:0,opacity:1},'fast',function(){
+				replacement.addClass('static');
+			});
+	}
+})(jQuery);
